c02a93ad6f78a884c68029a8cb4be1e329ef2e92
Yukti Marwah <yuktimarwahgithub@gmail.com>
2026-07-05 12:46:52 +0000

    recursion:countOfDigits.java added

---
+++ recursion/countOfDigits.java
@@ -0,0 +1,10 @@
+class countOfDigits{
+        public static int count(int n){
+                if(n==0) return 0;
+
+                return  count(n/10)+1;
+        }
+        public static void main(String[] args){
+                System.out.println(count(11112345));
+        }
+}
