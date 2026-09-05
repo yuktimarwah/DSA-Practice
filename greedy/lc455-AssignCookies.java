84d1cc0a56acfd46e764f2bd65df07b65d99f6f1
Yukti Marwah <yuktimarwahgithub@gmail.com>
2026-09-05 19:02:59 +0000

    lc 455 - Assign Cookies

---
+++ greedy/lc455-AssignCookies.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+
+        int contentChild = 0;
+        Arrays.sort(g);
+        Arrays.sort(s);
+        
+        int i = 0;
+        int j = 0;
+
+        while (i < g.length && j < s.length) {
+            if (g[i] <= s[j]){
+                contentChild++;
+                i++;
+            j++;
+            }
+            else {
+                j++;
+            }
+        }
+        return contentChild;
+    }
+}
