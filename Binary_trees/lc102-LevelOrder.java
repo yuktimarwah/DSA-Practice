c48eb54b54fa9ec419842d61a2c8375ec3f86447
Yukti Marwah <yuktimarwahgithub@gmail.com>
2026-09-19 10:38:53 +0000

    lc 102 - Level Order Traversal

---
+++ Binary_trees/lc102-LevelOrder.java
@@ -0,0 +1,31 @@
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+
+        if (root == null) {
+            return res;
+        }
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+
+        while (!q.isEmpty()) {
+            int size = q.size();
+            
+            List<Integer> level = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode currNode = q.poll();
+                level.add(currNode.val);
+
+                if (currNode.left != null) {
+                    q.add(currNode.left);
+                }
+                if (currNode.right != null) {
+                    q.add(currNode.right);
+                }
+            }
+            res.add(level);  
+        }
+        return res;
+    }
+}
