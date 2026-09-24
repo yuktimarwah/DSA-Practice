8b7a952d2c63b807632b497fcd6c338874c2c782
Yukti Marwah <yuktimarwahgithub@gmail.com>
2026-09-24 14:00:57 +0000

    lc 153 - Find Min In Rotated Sorted Array

---
+++ Binary_Search/lc153-FindMinInRotatedSortedArray.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findMin(int[] nums) {
+        int l = 0;
+        int r = nums.length - 1;
+
+        while (r > l) {
+            int mid = l + (r - l) / 2;
+
+            if (nums[mid] >= nums[r]) {
+                l = mid + 1;
+            }
+            else {
+                r = mid;
+            }
+        }
+        return nums[l];
+    }
+}
